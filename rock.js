e6de19bca1cc290e5fed85baf8a92d78e88dbcda
andrea-enrica <enricaiordache@gmail.com>
2020-11-05 18:31:59 +0100

    Added css and updated html and js

--- rock.js
+++ rock.js
@@ -1,69 +1,135 @@
 //Radom computer selection
 function computerPlay () {
-    var array = ["rock", "paper", "scissors"];
-    var result = (Math.floor(Math.random()*(2-0+1)));
-    return array[result];
-}//end computerPlay
+    var possibleChoices = ["Rock", "Paper", "Scissors"];
+    var computerSelection = (Math.floor(Math.random()*(2-0+1)));
+    return possibleChoices[computerSelection];
+}
 
 // playing one round
-function playRound() {
-    var player = prompt("Let's play Rock, Paper and Scissors!").toLowerCase();
+function playRound(selection) {
+    var player = selection;
     var computer = computerPlay();
     var gameResult = "";
-
-    if (player === "rock" && computer === "rock") {
+   
+    if (player === "Rock" && computer === "Rock") {
         gameResult = "tie";
-        alert("It was a tie. Try again!");
-    } else if(player === "rock" && computer === "paper") {
+        roundResult.textContent = "Tie round! No winners.";
+    } else if(player === "Rock" && computer === "Paper") {
         gameResult = "lose";
-        alert("You lost the round!");
-    } else if (player === "rock" && computer === "scissors") {
+        roundResult.textContent = "You lose! Paper covers rock.";
+    } else if (player === "Rock" && computer === "Scissors") {
         gameResult = "win";
-        alert("You won the round!");
-    } else if (player === "paper" && computer === "paper") {
+        roundResult.textContent = "You won! Rock smashes scissors.";
+    } else if (player === "Paper" && computer === "Paper") {
         gameResult = "tie";
-        alert("It was a tie. Try again!");
-    } else if (player === "paper" && computer === "rock") {
+        roundResult.textContent = "Tie round! No winners.";
+    } else if (player === "Paper" && computer === "Rock") {
         gameResult = "win";
-        alert("You won the round!");
-    } else if (player === "paper" && computer === "scissors") {
+        roundResult.textContent = "You won! Paper covers rock.";
+    } else if (player === "Paper" && computer === "Scissors") {
         gameResult = "lose";
-        alert("You lost the round!");
-    } else if (player === "scissors" && computer === "scissors") {
+        roundResult.textContent = "You lose! Scissors cuts paper!";
+    } else if (player === "Scissors" && computer === "Scissors") {
         gameResult = "tie";
-        alert("It was a tie. Try again!");
-    } else if (player === "scissors" && computer === "rock") {
+        roundResult.textContent = "Tie round! No winners.";
+    } else if (player === "Scissors" && computer === "Rock") {
         gameResult = "lose";
-        alert("You lost the round!");
-    } else if (player === "scissors" && computer === "paper") {
+        roundResult.textContent = "You lose! Rock smashes scissors.";
+    } else if (player === "Scissors" && computer === "Paper") {
         gameResult = "win";
-        alert("You won the round!");
-    } else {
-        alert("Invalid Input! Please type Rock, Paper or Scissors!")
-    }
-    return gameResult;
-} //end playRound
-
-// playing 5 rounds
-function game() {
-    for (i=1; i<=5; i++) {
-        var roundResult = playRound();
-        var playerScore = 0; 
-
-        if (roundResult === "win") {
-            playerScore++;
-        } else if (roundResult === "lose") {
-            playerScore--;
-        } else {
-            playRound();
-        }
+        roundResult.textContent = "You won! Scissors cuts paper.";
     } 
-    
-    if (playerScore < 1) {
-        alert("You lose! Your score was: " + playerScore)
-    } else {
-        alert("You win! Your score was: " + playerScore)
+    return gameResult;
+}
+
+//create elements
+const container = document.querySelector('body > div.container');
+const roundResult = document.createElement('div');
+const roundSelection = document.createElement('div');
+const playerScoreElement = document.createElement('div');
+const computerScoreElement = document.createElement('div');
+const finalButton = document.createElement('button');
+
+roundResult.classList.add('hidden');
+roundResult.setAttribute('id', 'roundResult');
+roundSelection.classList.add('hidden');
+roundSelection.setAttribute('id', 'roundSelection');
+playerScoreElement.classList.add('score');
+computerScoreElement.classList.add('score');
+finalButton.classList.add('hidden');
+finalButton.setAttribute('id', 'finalButton')
+
+let roundCount = 0;
+var playerScore = 0; 
+var computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        game(button);
+        button.classList.add('playing');
+        setTimeout(() => {  button.classList.remove('playing');}, 150);
+   });
+});
+
+//plays 5 rounds and displays info to page
+function game(button) {
+    if (roundCount < 4){
+        roundPlayAndScore(button);
+    } else if (roundCount >=4 && roundCount <5) {  
+        roundPlayAndScore(button);
+        if (playerScore < 1) {
+            finalButton.textContent = "You lose! Better luck next time!"
+            finalButton.classList.remove('hidden');
+        } else {
+            finalButton.textContent = "Congratulation, you won! Want to play again?"
+            finalButton.classList.remove('hidden');
+        }
+    resetGame();
     }
 }
 
-game();
+function roundPlayAndScore(button) {
+    roundSelection.textContent = ("You chose: " + button.id);
+    var oneRoundResult = playRound(button.id);
+    container.appendChild(roundSelection);
+    container.appendChild(roundResult);
+
+    if (oneRoundResult === "win") {
+        roundCount++;
+        playerScore++;
+        computerScore--;
+    } else if (oneRoundResult === "lose") {
+        roundCount++;
+        playerScore--;
+        computerScore++;
+    } else {
+        roundCount = roundCount;
+        playerScore = playerScore;
+    }
+    
+    playerScoreElement.textContent = ("Player score: " + playerScore);
+    computerScoreElement.textContent = ("Computer score: " + computerScore);
+    container.appendChild(playerScoreElement);
+    container.appendChild(computerScoreElement);
+
+    roundSelection.classList.remove('hidden');
+    roundResult.classList.remove('hidden');
+}
+
+function resetGame() {
+    container.appendChild(finalButton);
+    finalButton.addEventListener('click', () => {
+        roundCount = 0;
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreElement.textContent = ("Player score: " + playerScore);
+        computerScoreElement.textContent = ("Computer score: " + computerScore);
+        container.appendChild(playerScoreElement);
+        container.appendChild(computerScoreElement);
+
+        roundSelection.classList.add('hidden');
+        roundResult.classList.add('hidden');
+        finalButton.classList.add('hidden');
+    });
+}
